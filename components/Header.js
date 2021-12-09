9084112392fe64c9a3bdba0a6a8b522677a9342b
Salvador Tena <salvador_tena@hotmail.com>
2021-12-09 01:03:23 -0800

    modified the navbar with a toggle function

--- components/Header.js
+++ components/Header.js
@@ -11,6 +11,7 @@ function Header() {
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Scope+One&display=swap" rel="stylesheet" />
+                <link href="https://fonts.googleapis.com/css2?family=Scope+One&display=swap" rel="stylesheet" />
             </Head>
         </div>
 
