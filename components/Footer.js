4f843682ef95dc72102a5c43139e2f1ed294518f
Salvador Tena <salvador_tena@hotmail.com>
2021-12-10 00:57:47 -0800

    added brand icons

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,17 @@
+import styles from './footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faIndustry } from '@fortawesome/free-solid-svg-icons'
 
 
 function Footer() {
     return (
-        <h1>Footer</h1>
+        <div>
+            <footer className={styles.container}>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="far fa-copyright">2021</i>
+            </footer>
+        </div>
     )
 }
 
